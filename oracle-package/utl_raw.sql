100b03b2b9980d3550d22a92a4cb98bfdb3fe7b2
DarkAthena <darkathena@qq.com>
2022-02-03 22:00:24 +0800

    add

---
+++ oracle-package/utl_raw.sql
@@ -0,0 +1,85 @@
+create schema UTL_RAW;
+
+CREATE OR REPLACE FUNCTION UTL_RAW.cast_to_varchar2(r IN BYTEA)
+RETURNS text
+LANGUAGE SQL
+IMMUTABLE NOT FENCED NOT SHIPPABLE
+AS $$
+SELECT convert_from(r,'UTF8');
+ $$;
+/
+
+CREATE OR REPLACE FUNCTION UTL_RAW.cast_to_raw(c IN text)
+RETURNS BYTEA
+LANGUAGE SQL
+IMMUTABLE NOT FENCED NOT SHIPPABLE
+AS $$
+SELECT c::BYTEA;
+ $$;
+/
+
+CREATE OR REPLACE FUNCTION UTL_RAW.concat(
+r1  IN BYTEA DEFAULT ''::BYTEA,
+r2  IN BYTEA DEFAULT ''::BYTEA,
+r3  IN BYTEA DEFAULT ''::BYTEA,
+r4  IN BYTEA DEFAULT ''::BYTEA,
+r5  IN BYTEA DEFAULT ''::BYTEA,
+r6  IN BYTEA DEFAULT ''::BYTEA,
+r7  IN BYTEA DEFAULT ''::BYTEA,
+r8  IN BYTEA DEFAULT ''::BYTEA,
+r9  IN BYTEA DEFAULT ''::BYTEA,
+r10 IN BYTEA DEFAULT ''::BYTEA,
+r11 IN BYTEA DEFAULT ''::BYTEA,
+r12 IN BYTEA DEFAULT ''::BYTEA
+)
+RETURNS BYTEA
+LANGUAGE SQL
+IMMUTABLE NOT FENCED NOT SHIPPABLE
+AS $$
+SELECT r1||r2||r3||r4||r5||r6||r7||r8||r9||r10||r11||r12;
+ $$;
+/
+
+CREATE OR REPLACE FUNCTION UTL_RAW.length(r IN bytea)
+RETURNS int4
+LANGUAGE SQL
+IMMUTABLE NOT FENCED NOT SHIPPABLE
+AS $$
+SELECT length(r);
+ $$;
+/
+
+CREATE OR REPLACE FUNCTION UTL_RAW.substr(r IN bytea,pos in int4,len in int4 DEFAULT null)
+RETURNS bytea
+LANGUAGE SQL
+IMMUTABLE NOT FENCED NOT SHIPPABLE
+AS $$
+SELECT case when len is not null then  substr(r,pos,len) else substr(r,pos) end ;
+ $$;
+/
+
+
+CREATE OR REPLACE FUNCTION UTL_RAW.translate(r IN bytea,from_set in bytea,to_set in bytea )
+RETURNS bytea
+LANGUAGE plpgsql
+IMMUTABLE NOT FENCED NOT SHIPPABLE
+AS $$
+DECLARE
+l_tmp text;
+begin
+l_tmp:=regexp_replace(REPLACE(r::TEXT,'\x',''), '(..)','=\1', 'g');
+for rec in (
+select fs_str,nvl(ts_str,'') ts_str from 
+(select row_number() over() fs_pos,fs_str from (
+select (regexp_matches(REPLACE(from_set::TEXT,'\x',''), '(..)', 'g')) [ 1 ] fs_str)) f
+left join 
+(select row_number() over() ts_pos,ts_str from (
+select (regexp_matches(REPLACE(to_set::TEXT,'\x',''), '(..)', 'g')) [ 1 ] ts_str)) t
+on f.fs_pos=t.ts_pos
+) loop
+l_tmp:=replace(l_tmp,'='||rec.fs_str,rec.ts_str);
+end loop;
+return decode(replace(l_tmp,'=',''),'HEX');
+end;
+$$;
+/
