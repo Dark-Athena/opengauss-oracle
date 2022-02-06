82cf622e8235f925b1318be8b0532df401ac4b08
DarkAthena <darkathena@qq.com>
2022-02-06 17:16:46 +0800

    Update unistr.sql
    add "quote_nullable" to avoid SQL Injection

--- oracle-function/unistr.sql
+++ oracle-function/unistr.sql
@@ -1,12 +1,19 @@
 CREATE OR REPLACE FUNCTION pg_catalog.unistr(text)
-RETURNS text
-LANGUAGE plpgsql
-NOT FENCED NOT SHIPPABLE
+ RETURNS text
+ LANGUAGE plpgsql
+ NOT FENCED NOT SHIPPABLE
 AS $$
 DECLARE
   r text;
 begin
-  EXECUTE  'select e'''||$1||'''' into r;
+IF LENGTH($1)>0 THEN 
+  EXECUTE left(REPLACE(REPLACE('select '||'U&'''||quote_nullable($1)||'''','U&''E''','U&'''),'\\','\'),-1) into r;
+ELSe r:=str;
+END IF;
   return r;
 end; $$;
 /
+
+--select unistr('\80e1\6843');
+--防sql注入
+--select unistr('\80e1\6843'';select ''1  ');
