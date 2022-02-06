ce00070f5ed5ea670bed3e354aa70b96a2fb0bde
DarkAthena <darkathena@qq.com>
2022-02-06 17:15:31 +0800

    Update asciistr.sql
    fix "\"

--- oracle-function/asciistr.sql
+++ oracle-function/asciistr.sql
@@ -1,11 +1,10 @@
---带了个常见形式的u
 CREATE OR REPLACE FUNCTION pg_catalog.asciistr(text)
-RETURNS text
-LANGUAGE sql
-NOT FENCED NOT SHIPPABLE
+ RETURNS text
+ LANGUAGE sql
+ NOT FENCED NOT SHIPPABLE
 AS $$
-select string_agg( (case when ascii(s)<=255 then s else 
-'\u'||(to_hex(ascii(s))::text) end ),'')
+select string_agg( '\'||lpad((case when ascii(s)<=255 and s!='\' then s else 
+(to_hex(ascii(s))::text) end ),4,'0'),'')
   from (select unnest(string_to_array($1, null) ) s);
  $$;
 /
