0e706f55d01ee3284c8bddcaaefd2543b46ccfa6
DarkAthena <darkathena@qq.com>
2022-02-09 20:34:13 +0800

    Update asciistr.sql

--- oracle-function/asciistr.sql
+++ oracle-function/asciistr.sql
@@ -3,8 +3,8 @@ CREATE OR REPLACE FUNCTION pg_catalog.asciistr(text)
  LANGUAGE sql
  NOT FENCED NOT SHIPPABLE
 AS $$
-select string_agg( '\'||lpad((case when ascii(s)<=255 and s!='\' then s else 
-(to_hex(ascii(s))::text) end ),4,'0'),'')
+select string_agg( (case when ascii(s)<=255 and s!='\' then s else 
+'\'||lpad((to_hex(ascii(s))::text),4,'0') end ),'')
   from (select unnest(string_to_array($1, null) ) s);
  $$;
 /
