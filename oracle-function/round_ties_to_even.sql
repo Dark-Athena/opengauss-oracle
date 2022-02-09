f238c2f45a46a7625c054db2b26817eeb23e6297
DarkAthena <darkathena@qq.com>
2022-02-09 20:08:43 +0800

    Update round_ties_to_even.sql

--- oracle-function/round_ties_to_even.sql
+++ oracle-function/round_ties_to_even.sql
@@ -1,12 +1,20 @@
-CREATE OR REPLACE FUNCTION pg_catalog.round_ties_to_even(numeric)
+create or replace function pg_catalog.round_ties_to_even(n NUMERIC,places int4 DEFAULT 0)
 RETURNS numeric
-LANGUAGE sql
+LANGUAGE plpgsql
 IMMUTABLE STRICT NOT FENCED NOT SHIPPABLE COST 1
 AS $$
-  select case
-           when $1 - pg_catalog.trunc($1, 0) = 0.5
-                     and mod(pg_catalog.trunc($1, 0), 2) = 0 then
-             $1 - 0.5
-           else
-             pg_catalog.round($1, 0) end$$; 
+declare
+l_ret numeric;
+l_dif numeric;
+begin
+l_ret := round(n,places);
+l_dif := l_ret-n;
+if abs(l_dif)*(10^places) = 0.5 then
+if not (l_ret * (10^places)) % 2 = 0 then
+l_ret := round(n-l_dif,places);
+end if;
+end if;
+return l_ret;
+end;
+$$;
 /
